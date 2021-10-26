70a678c5ce71905f2bffbc2647a791e5f3267e17
Xde44 <92163047+Xde44@users.noreply.github.com>
2021-10-26 22:52:39 +0200

    Add files via upload

---
+++ wyle.py
@@ -0,0 +1,138 @@
+print("""
+██╗░░██╗░██████╗██████╗░
+╚██╗██╔╝██╔════╝╚════██╗
+░╚███╔╝░╚█████╗░░█████╔╝
+░██╔██╗░░╚═══██╗░╚═══██╗
+██╔╝╚██╗██████╔╝██████╔╝
+╚═╝░░╚═╝╚═════╝░╚═════╝░
+
+░█████╗░░██████╗██╗███╗░░██╗████████╗
+██╔══██╗██╔════╝██║████╗░██║╚══██╔══╝
+██║░░██║╚█████╗░██║██╔██╗██║░░░██║░░░
+██║░░██║░╚═══██╗██║██║╚████║░░░██║░░░
+╚█████╔╝██████╔╝██║██║░╚███║░░░██║░░░
+░╚════╝░╚═════╝░╚═╝╚═╝░░╚══╝░░░╚═╝░░░""")
+import time 
+time.sleep(2)
+print()
+print()
+print("""1)Osint
+             
+             
+             """)
+eligio=input("Pon el numero:")
+ 
+if eligio=="1":
+ 	nombre=input("Pon un nombre o apellido (todo junto):")
+ 	print()
+ 	print("-------------------------------")
+ 	print("Redes Principales")
+ 	print("-------------------------------")
+ 	print()
+print("https://www.pinterest.es/"+nombre+"")  
+print("https://www.facebook.com/"+nombre+"")           
+print("https://www.instagram.com/"+nombre+"")
+ 
+print("https://www.youtube.com/"+nombre+"")
+print("https://github.com/"+nombre+"")
+print("https://open.spotify.com/user/"+nombre+"")
+ 
+print("https://tiktok.com/@"+nombre+"")
+
+print("https://www.linkedin.com/"+nombre+"")
+print("https://mobile.twitter.com/"+nombre+"")
+print("https://steamcomunity/id/"+nombre+"")
+print("https://t.me/"+nombre+"")
+print("https//:www.gotinder.com/"+nombre+"")
+print("htps//:vsco.co/"+nombre+"")
+print("https://www.paypal.com/"+nombre+"")
+print("https://keybase.io/"+nombre+"")
+print("https://tinder.com"+nombre+"")
+import time 
+time.sleep(2)
+print()
+print("------------------------------")
+print("Posibles Correos") 
+print("-------------------------------")
+print()
+print(""+nombre+"@gmail.com")
+print(""+nombre+"@yahoo.com")
+print(""+nombre+"@Outlook.com")
+print(""+nombre+"@GMX.com")
+print(""+nombre+"@AOL.com")
+print()
+print(""+nombre+"@gmail.es")
+print(""+nombre+"@yahoo.es")
+print(""+nombre+"@Outlook.es")
+print(""+nombre+"@GMX.es")
+print(""+nombre+"@AOL.es")
+print()
+print(""+nombre+"@gmail.net")
+print(""+nombre+"@yahoo.net")
+print(""+nombre+"@Outlook.net")
+print(""+nombre+"@GMX.net")
+print(""+nombre+"@AOL.net")
+import time 
+time.sleep(2)
+print()
+print("---------------------------")
+print("Posibles Dominios")
+print("------------------------------")
+print()
+print(""+nombre+".com")
+print(""+nombre+".net")
+print(""+nombre+".org")
+print(""+nombre+".co")
+print(""+nombre+".biz")
+print(""+nombre+".io")
+print(""+nombre+".at")
+print(""+nombre+".us")
+print(""+nombre+".me")
+print(""+nombre+".co.uk")
+print(""+nombre+".eu")
+print(""+nombre+".info")
+print(""+nombre+".xyz")
+print(""+nombre+".live")
+print(""+nombre+".pro")
+print(""+nombre+".am")
+print(""+nombre+".tv")
+print(""+nombre+".shop")
+print(""+nombre+".life")
+print(""+nombre+".ch")
+print(""+nombre+".today")
+print(""+nombre+".in")
+print(""+nombre+".club")
+print(""+nombre+".cc")
+print(""+nombre+".tech")
+print(""+nombre+".site")
+print(""+nombre+".online")
+print(""+nombre+".store")
+print(""+nombre+".space")
+print(""+nombre+".website")
+print(""+nombre+".vip")
+print(""+nombre+".host")
+print(""+nombre+".press")
+print(""+nombre+".digital")
+print(""+nombre+".guru")
+print(""+nombre+".de")
+print(""+nombre+".ltd")
+print(""+nombre+".tk")
+print(""+nombre+".nl")
+print(""+nombre+".ca")
+print(""+nombre+".tw")
+print(""+nombre+".fun")
+print(""+nombre+".ws")
+print(""+nombre+".work")
+print(""+nombre+".tools")
+input()
+
+
+
+
+
+
+
+
+ 
+ 	
+ 
